f6f86f422a662f4441e113bfe626c17d2f1718f6
Grzegorz Gawron <grzegorzgawron@gmail.com>
2016-12-12 18:40:07 +0100

    adding some sample data

--- Tweeterator/tweeterator.py
+++ Tweeterator/tweeterator.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from brain import Brain
 from utils import data_path
 
@@ -60,7 +61,7 @@ def compare_iteration(model_prefix, iterations, diversities, training_text, seed
         for (ix, name, div), generated in sorted(result.iteritems()):
             print "ix={}, model={}, div={}| {}".format(ix, name, div, generated.encode('utf-8'))
 
-train(maxlens=[40], steps=[4], dropouts=[0.1], dataset=filter(lambda x: x[0]=='italian', datasets.iteritems()))
-compare_iteration('11,italian,tweets-40-2-128-0.1_', iterations=[5], diversities=[0.4]*10, training_text=datasets['italian'])
+# train(maxlens=[40], steps=[4], dropouts=[0.1], dataset=filter(lambda x: x[0]=='italian', datasets.iteritems()))
+compare_iteration('11,trump,tweets-40-2-128-0.1_', iterations=[5], diversities=[0.4]*2, training_text=datasets['trump'])
 # compare_iteration('11,pis,tweets-60-8-128-0.1_', iterations=[9], diversities=[0.4]*30, training_text=datasets['pis'])
 
